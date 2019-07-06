aca2743c8d35d0a85a36168acf84123a68f805f8
Daniel Hardman <daniel.hardman@gmail.com>
2019-07-05 19:52:36 -0600

    Fix js example
    Signed-off-by: Daniel Hardman <daniel.hardman@gmail.com>

--- compare.js
+++ compare.js
@@ -1,18 +1,15 @@
 function comparePeerDIDs(didA, didB) {
-    prefixA = didA.substring(0,12);
-    console.log(prefixA);
-    prefixB = didB.substring(0,12);
+    a = didA.substring(0,12);
+    b = didB.substring(0,12);
     // Compare the prefix case-sensitively.
-    n = (prefixA < prefixB) ? -1 : (prefixA > prefixB ? 1 : 0);
-    console.log(n);
+    n = (a < b) ? -1 : (a > b ? 1 : 0);
     if (n == 0) {
         // Don't use .localeCompare() or .toLocaleLowerCase();
         // we want raw ASCII comparison.
-        numericA = didA.substring(12).toLowerCase();
-        numericB = didB.substring(12).toLowerCase();
+        a = didA.substring(12).toLowerCase();
+        b = didB.substring(12).toLowerCase();
         // Compare the numeric portion case-insensitively.
-        n = numericA.localeCompare(numericB);
+        n = (a < b) ? -1 : (a > b ? 1 : 0);
     }
-    // Return < 0 if a < b, 0 if a == b, > 0 if a > b
     return n;
 }
