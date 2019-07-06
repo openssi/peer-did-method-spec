a48c3f30dbb9b816367de4c06f2aa3f1a296883e
Daniel Hardman <daniel.hardman@gmail.com>
2019-07-06 12:53:34 -0600

    More detail about levels of implementation
    Signed-off-by: Daniel Hardman <daniel.hardman@gmail.com>

--- compare.js
+++ compare.js
@@ -5,10 +5,10 @@ function comparePeerDIDs(didA, didB) {
     n = (a < b) ? -1 : (a > b ? 1 : 0);
     if (n == 0) {
         // Don't use .localeCompare() or .toLocaleLowerCase();
-        // we want raw ASCII comparison.
+        // we want raw ASCII comparison. Just normalize to
+        // lower case before comparing.
         a = didA.substring(12).toLowerCase();
         b = didB.substring(12).toLowerCase();
-        // Compare the numeric portion case-insensitively.
         n = (a < b) ? -1 : (a > b ? 1 : 0);
     }
     return n;
